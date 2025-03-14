2b068141bdf566d265de80cfd3c111ec0867a37e
Benny14-14 <Benny14.14.stream@gmail.com>
2025-03-15 00:04:47 +0100

    Create accounts.js

---
+++ dnd/latest/accounts.js
@@ -0,0 +1,12 @@
+{
+    "Benutzer1": { "password": "passwort1", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler1.html" },
+    "Benutzer2": { "password": "passwort2", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler2.html" },
+    "Benutzer3": { "password": "passwort3", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler3.html" },
+    "Benutzer4": { "password": "passwort4", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler4.html" },
+    "Benutzer5": { "password": "passwort5", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler5.html" },
+    "Benutzer6": { "password": "passwort6", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler6.html" },
+    "Benutzer7": { "password": "passwort7", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler7.html" },
+    "Benutzer8": { "password": "passwort8", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler8.html" },
+    "Benutzer9": { "password": "passwort9", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler9.html" },
+    "Benutzer10": { "password": "passwort10", "redirect": "https://benny14-14.github.io/Games/dnd/latest/player/spieler10.html" }
+}
