04ef853f560494a95114813ac21699363cb4ba0f
Benny14-14 <Benny14.14.stream@gmail.com>
2025-03-19 15:33:20 +0100

    Create ui.js

---
+++ web/snake/ui.js
@@ -0,0 +1,39 @@
+function drawGrid() {
+    ctx.strokeStyle = "#444";
+    for (let i = 0; i < canvas.width / gridSize; i++) {
+        for (let j = 0; j < canvas.height / gridSize; j++) {
+            ctx.strokeRect(i * gridSize, j * gridSize, gridSize, gridSize);
+        }
+    }
+}
+
+function drawSnake() {
+    ctx.fillStyle = "lime";
+    snake.forEach(segment => {
+        ctx.fillRect(segment.x * gridSize, segment.y * gridSize, gridSize, gridSize);
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(segment.x * gridSize, segment.y * gridSize, gridSize, gridSize);
+    });
+}
+
+function drawFood() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x * gridSize, food.y * gridSize, gridSize, gridSize);
+}
+
+function drawGame() {
+    ctx.fillStyle = "#222";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
+    drawFood();
+    drawSnake();
+}
+
+function updateScore() {
+    document.getElementById("score").textContent = score;
+    if (score > highscore) {
+        highscore = score;
+        localStorage.setItem("highscore", highscore);
+        document.getElementById("highscore").textContent = highscore;
+    }
+}
